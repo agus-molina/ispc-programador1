cffdac80c3a6a972990fe3b90297e299b39b310d
Agus Molina <agus.molina1598@gmail.com>
2025-06-08 01:57:56 -0300

    archivos base para poder empezar a modularizar

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-'''  Actividad Integradora N° 2
+'''  Actividad Integradora N° 3
 Programación I
-Basándose en los requerimientos funcionales y no funcionales del proyecto, se
-solicita inicialmente:
-1. Realizar un programa modular que cumpla con los requerimientos generales
-del programa que son:
-a. Gestionar los dispositivos (listar, buscar, agregar y eliminar)
-b. Gestionar automatizaciones (sólo una).
-Nota: Almacenar la información en estructuras de datos (ej. listas,
-diccionarios) '''
+Permitir el registro de un nuevo usuario estándar e inicio de sesión.
+Si el usuario estándar inició sesión satisfactoriamente, permitir las siguientes acciones mediante un menú:
+Permitir consultar los datos personales.
+Permitir activar/ejecutar/”configurar”(debería estar predefinida) la automatización.
+Permitir consultar dispositivos.
+Si el usuario admin inició sesión satisfactoriamente, permitir las siguientes acciones mediante un menú:
+Permitir consultar automatizaciones activas
+Gestionar dispositivos.
+Permitir modificar el rol de un usuario. 
 
-from funciones import *
+** El usuario Admin, puede ser el primer usuario que se registra.**
+ '''
+
+from funciones import (
+    modo_actual_programa,
+    mostrar_menu,
+    listar_dispositivos,
+    buscar_dispositivo,
+    agregar_dispositivo,
+    eliminar_dispositivo,
+    ajuste_automatico
+)
 
 def main():
     print('*** BIENVENIDO AL SISTEMA SMART SOLUTIONS ***')
@@ -24,53 +36,40 @@ def main():
 
     while login:
         mostrar_menu()
-        seleccion_valida = False
-        while not seleccion_valida:
-            entrada = input("Seleccione el número de la opción deseada: ").strip()
-            if entrada.isdigit():
-                seleccion = int(entrada)
-                seleccion_valida = True
-            else:
-                print("Entrada inválida. Por favor, ingrese un número correspondiente a las opciones del menú.\n")
-
-        seleccion = int(input("Seleccione el número de la opción deseada: ").strip())
-        match seleccion:
-            case 1:
-                listar_dispositivos(inventario)
-            case 2:
-                busqueda = input('Ingresa el "NOMBRE" del Dispositivo a buscar: ').strip().lower()
-                resultado = buscar_dispositivo(inventario, busqueda)
-                if resultado:
-                    print('Dispositivo Encontrado:')
-                    print(f'''ID: {resultado.get('id')}
-                    Nombre: {resultado.get('nombre')}
-                    Tipo: {resultado.get('tipo')}
-                    Estado: {resultado.get('estado')}''')
-                else:
-                    print('Dispositivo no encontrado...')
-            case 3:
-                agregar_dispositivo(inventario, contador_id)
-                contador_id += 1
-            case 4:
-                if inventario:
-                    print("\n*** Dispositivos disponibles para eliminar ***")
-                    for dispositivo in inventario:
-                        print(f"ID: {dispositivo.get('id')} - Nombre: {dispositivo.get('nombre')}")
-                    try:
+        try:
+            seleccion = int(input("Seleccione el número de la opción deseada: ").strip())
+            match seleccion:
+                case 1:
+                    listar_dispositivos(inventario)
+                case 2:
+                    busqueda = input('Ingresa el "NOMBRE" del Dispositivo a buscar: ').strip().lower()
+                    resultado = buscar_dispositivo(inventario, busqueda)
+                    if resultado:
+                        print('Dispositivo Encontrado:')
+                        print(f'''ID: {resultado.get('id')}
+                        Nombre: {resultado.get('nombre')}
+                        Tipo: {resultado.get('tipo')}
+                        Estado: {resultado.get('estado')}''')
+                    else:
+                        print('Dispositivo no encontrado...')
+                case 3:
+                    agregar_dispositivo(inventario, contador_id)
+                    contador_id += 1
+                case 4:
                         id_a_eliminar = int(input('Ingrese la ID del dispositivo a Desvincular: ').strip())
                         eliminado = eliminar_dispositivo(inventario, id_a_eliminar)
                         if eliminado:
                             print(f'Dispositivo con ID {id_a_eliminar} eliminado correctamente.')
                         else:
                             print(f'No se encontró un dispositivo con ID {id_a_eliminar}.')
-                    except ValueError:
-                        print('Por favor, ingrese un número válido para la ID.')
-            case 5:
-                print('Muchas gracias por utilizar Smart Solutions...')
-                print('Saliendo del sistema...')
-                login = False
-            case _:
-                print("Entrada inválida. Por favor, ingrese un número que corresponda\n")
+                case 5:
+                    print('Muchas gracias por utilizar Smart Solutions...')
+                    print('Saliendo del sistema...')
+                    login = False
+                case _:
+                    print("Opción inválida. Por favor, ingrese un número que corresponda\n")
+        except ValueError:
+            print("Opción inválida.\n")
 
 if __name__ == "__main__":
     main()
