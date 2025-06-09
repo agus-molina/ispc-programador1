ffe7b4e6a5b92f4d7a2c208191e429720271e44a
Marysol Ranieri <lar_sol22@live.com>
2025-06-08 22:28:17 -0300

    agrego menu de admi

--- menu.py
+++ menu.py
@@ -30,3 +30,42 @@ def menu_usuario_estandar(usuario, inventario):
             break
         else:
             print("Opción inválida.")
+
+def menu_admin(usuarios, inventario):
+    while True:
+        print(f'''\n--- Menú ADMIN ---
+            1. Consultar automatizaciones activas
+            2. Gestionar dispositivos")
+            3. Modificar rol de usuario")
+            4. Cerrar sesión\n''')
+
+        opcion = input("Seleccione una opción: ").strip()
+        match opcion:
+            case '1':
+                ''' '''
+            case '2':
+                print(f'''\n--- Gestión de Dispositivos: ---
+                1. Listar
+                2. Agregar Nuevo
+                3. Eliminar
+                4. Buscar\n''')
+
+                opcion = input("Seleccione una opción: ").strip()
+                match opcion:
+                    case 1:
+                        listar_dispositivos(inventario)
+                    case 2:
+                        agregar_dispositivo(inventario, contador_id)
+                    case 3:
+                        eliminar_dispositivo(inventario)
+                    case 4:
+                        buscar_dispositivo(inventario, nombre)
+                    case _:
+                        print("Opción inválida.")
+            case '3':
+                ''' '''
+            case '4':
+                print("Cerrando sesión...\n")
+                break
+            case _:
+                print("Opción inválida.")
