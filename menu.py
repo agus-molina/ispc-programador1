a296147a27bf0497b8d698b6018db1c75c258387
Marysol Ranieri <lar_sol22@live.com>
2025-06-08 22:24:28 -0300

    se agrega menu de usuario estandar

--- menu.py
+++ menu.py
@@ -2,10 +2,31 @@
 MODULO DE PRESENTACION DEL MENU
 '''
 
-def mostrar_menu():
-    print('''\n         _Menú Principal_ 
-        1) Listar Dispositivos Vinculados
-        2) Buscar Dispositivos
-        3) Agregar Nuevo Dispositivo
-        4) Eliminar Dispositivo
-        5) Salir del Sistema''')
+from admin_dispositivos import (
+    listar_dispositivos,
+    buscar_dispositivo,
+    agregar_dispositivo,
+    eliminar_dispositivo,
+    ejecutar_automatizacion
+)
+
+from usuarios import mostrar_datos_personales
+def menu_usuario_estandar(usuario, inventario):
+    while True:
+        print(f'''\n--- Menú Usuario: {usuario['nombre']} ---
+            1. Consultar datos personales")
+            2. Ejecutar automatización")
+            3. Consultar dispositivos")
+            4. Cerrar sesión''')
+        opcion = input("Seleccione una opción: ").strip()
+        if opcion == "1":
+            mostrar_datos_personales(usuario)
+        elif opcion == "2":
+            ejecutar_automatizacion(inventario)
+        elif opcion == "3":
+            listar_dispositivos(inventario)
+        elif opcion == "4":
+            print("Cerrando sesión...\n")
+            break
+        else:
+            print("Opción inválida.")
