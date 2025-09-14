89348f32a7a6cd58b1f8a42aedd74e02dffbeba7
Agus Molina <agus.molina1598@gmail.com>
2025-09-14 00:58:36 -0300

    15 de 30 inserts agregados

--- BD-Evidencia-5/dll.sql
+++ BD-Evidencia-5/dll.sql
@@ -12,8 +12,8 @@ CREATE TABLE dispositivos (
     nombre VARCHAR(255) NOT NULL UNIQUE,
     tipo VARCHAR(100) NOT NULL,
     estado BOOLEAN NOT NULL,
-    intensidad INT,
-    volumen INT,
+    intensidad INT UNSIGNED,
+    volumen INT UNSIGNED,
     infrarrojo BOOLEAN,
     id_usuario INT NOT NULL,
     PRIMARY KEY (id_dispositivo),
@@ -38,3 +38,24 @@ CREATE TABLE automatizaciones (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+INSERT INTO usuarios (username, contraseña, correo, rol)
+VALUES
+    ('susana', '123', 'hola@ispc.com', 'admin'),
+    ('clara', '1234', 'probando@ispc.com', 'admin'),
+    ('carlos', '1235', 'estoesunmail@ispc.com', 'estandar'),
+    ('pepe', '123456', 'email@ispc.com', 'estandar'),
+    ('meme', '1234567', 'correo@ispc.com', 'estandar'),
+    ('antonio', '12345678', 'posta@ispc.com', 'estandar');
+    
+INSERT INTO dispositivos (nombre, tipo, estado, intensidad, volumen, infrarrojo, id_usuario)
+VALUES
+    ('luz de living', 'luz', false, 0, NULL, NULL, 1),
+    ('luz de pieza principal', 'luz', true, 20, NULL, NULL, 1),
+    ('luz de comedor', 'luz', true, 10, NULL, NULL, 1),
+    ('televisor de pieza principal', 'electrodomestico', true, NULL, 15, NULL, 1),
+    ('televisor de living', 'electrodomestico', false, NULL, 0, NULL, 1),
+    ('camara patio', 'camara', true, NULL, NULL, true, 1),
+    ('camara cochera', 'camara', false, NULL, NULL, false, 1),
+    ('camara puerta', 'camara', true, NULL, NULL, true, 2),
+    ('luz oficina', 'luz', true, 30, NULL, NULL, 2);
