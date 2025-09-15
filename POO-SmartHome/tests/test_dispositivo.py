48e6c9fb6ae6622481476452dcf7ec8bfae3b698
Marysol Ranieri <lar_sol22@live.com>
2025-09-15 10:52:53 -0300

    unificacion y correccion en el otro archivo creado test

--- POO-SmartHome/tests/test_dispositivo.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from src.dispositivo import Dispositivo
-
-def test_encender_y_apagar():
-    d = Dispositivo(1, "Lámpara", "luces")
-    d.encender()
-    assert d.estado is True
-    d.apagar()
-    assert d.estado is False
