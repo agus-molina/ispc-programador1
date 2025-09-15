ed01d7132a1c331638e4b650b59c64b517727b94
Agus Molina <agus.molina1598@gmail.com>
2025-09-15 11:08:05 -0300

    cambio a metodos de clase dispositivo

--- POO-SmartHome/src/dispositivo.py
+++ POO-SmartHome/src/dispositivo.py
@@ -39,18 +39,41 @@ class Dispositivo:
         self.__estado = False
 
 class Camara(Dispositivo):
-    def __init__(self, tipo = "camara"):
-        super()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tipo="camara", **kwargs)
+
+    def interruptor_infrarrojo(self):
+        if self.__infrarrojo is not None:
+            self.__infrarrojo = not self.__infrarrojo
 
 class Luz(Dispositivo):
-    def __init__(self, tipo = "luz"):
-        super()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tipo="luz", **kwargs)
 
-    def subir_intensidad(self, cantidad):
-        pass
-    def bajar_intensidad(self, cantidad):
-        pass  
-    
+    def subir_intensidad(self, cantidad: int):
+        if self.__intensidad is not None:
+            self.__intensidad = min(100, self.__intensidad + cantidad)
+
+    def bajar_intensidad(self, cantidad: int):
+        if self.__intensidad is not None:
+            self.__intensidad = max(0, self.__intensidad - cantidad)
+
 class Electrodomestico(Dispositivo):
-    def __init__(self, tipo = "electrodomestico"):
-        super()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, tipo="electrodomestico", **kwargs)
+
+    def subir_volumen(self, cantidad: int):
+        if self.__volumen is not None:
+            self.__volumen = min(100, self.__volumen + cantidad)
+
+    def bajar_volumen(self, cantidad: int):
+        if self.__volumen is not None:
+            self.__volumen = max(0, self.__volumen - cantidad)
+
+    def subir_intensidad(self, cantidad: int):
+        if self.__intensidad is not None:
+            self.__intensidad = min(100, self.__intensidad + cantidad)
+
+    def bajar_intensidad(self, cantidad: int):
+        if self.__intensidad is not None:
+            self.__intensidad = max(0, self.__intensidad - cantidad)
